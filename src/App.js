a867b03378c6e6a66cb93b80d781fbf72bdc2b9e
Gord Bolli <gord@bolli.ca>
2022-10-14 15:03:22 -0700

    Move tasks data to state, setup usestate in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import { useState } from 'react'
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'doc appt',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true
+    },
+    {
+        id: 2,
+        text: 'think about life',
+        day: 'Feb 6th at 2:30pm',
+        reminder: false
+    },
+    {
+        id: 3,
+        text: 'run to the mailbox',
+        day: 'Feb 7th at 2:30pm',
+        reminder: true
+    },
+  ])
+
   return (
     <div className="container">
       <Header />
-      <Tasks />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
