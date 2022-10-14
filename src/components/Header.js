2f03d1f44cbeea036b2f1021a4570559e0e734cb
Gord Bolli <gord@bolli.ca>
2022-10-14 10:01:49 -0700

    Test inline styling in header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
   return (
     <div>
-        <h1>{title}</h1>
+        <h1 style={headingStyle}>{title}</h1>
     </div>
   )
 }
@@ -14,6 +14,11 @@ Header.defaultProps = {
 
 Header.propTypes = {
     title: PropTypes.string.isRequired
+}
+
+const headingStyle = {
+    color: 'Red',
+    backgroundColor: 'Black'
 }
 
 export default Header
