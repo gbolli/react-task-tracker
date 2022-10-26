8bf4aff0ccd7be6520a0cc77c05fc7253523eb01
Gord Bolli <gord@bolli.ca>
2022-10-26 10:33:06 -0700

    Add Footer to components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-import { useState, useEffect } from 'react'
+import Footer from './components/Footer'
 
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
@@ -83,6 +84,7 @@ function App() {
         />) :
         ('No tasks to show')
       }
+      <Footer />
     </div>
   )
 }
